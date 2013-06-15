81070e8568246eececb4a0f960a02c2c9f430ab2
Karthik Panjaje <karthikreadnovel@gmail.com>
2013-06-15 15:26:01 +0530

    Added configuration file support for Login Server

--- src/Login Server/Program.cs
+++ src/Login Server/Program.cs
@@ -53,17 +53,43 @@ namespace Login_Server
                 IPAddress loginServerIp = IPAddress.Any;
                 IPAddress gameServerIp = IPAddress.Parse("127.0.0.1");
                 _isRunning = true;
-                /*if(!File.Exists("LoginServer.ini"))
+                if(!File.Exists("LoginServer.ini"))
                     throw new FileNotFoundException("LoginServer.ini not found");
                 using (var streamReader = new StreamReader("LoginServer.ini", true))
                 {
-                    var readLine = streamReader.ReadLine();
-                }*/ //Config file support will be done in future release
-                Console.WriteLine(".:: Starting InfernoEmu Login Server ::.");
-                Console.Title = "InfernoEmu Login Server";
-                Console.WriteLine("Press ENTER key to activate command interface!");
+                    string readLine;
+                    while ((readLine = streamReader.ReadLine()) != null)
+                    {
+                        var config = readLine.Split(':');
+                        if(config.Length != 2)
+                            continue;
+                        switch (config[0])
+                        {
+                            case "LoginServerIp":
+                                loginServerIp = IPAddress.Parse(config[1].Trim());
+                                break;
+                            case "LoginServerPort":
+                                port = Convert.ToInt32(config[1].Trim());
+                                break;
+                            case "GameServerIp":
+                                gameServerIp = IPAddress.Parse(config[1].Trim());
+                                break;
+                            case "GameServerPort":
+                                gsPort = Convert.ToInt32(config[1].Trim());
+                                break;
+                            case "ServerName":
+                                serverName = config[1].Trim();
+                                break;
+                        }
+                    }
+                }
                 _loginServer = new Server(port, gsPort, loginServerIp, gameServerIp, serverName);
                 _loginServer.Start();
+                Console.WriteLine(".:: Starting InfernoEmu Login Server ::.");
+                Console.Title = "InfernoEmu Login Server";
+                Console.WriteLine("Server started with IP {0} , port {1} and server name {2}!", loginServerIp, port, serverName);
+                Console.WriteLine("Game server IP is {0} and port is {1}", gameServerIp, gsPort);
+                Console.WriteLine("Press ENTER key to activate command interface!");
                 Console.Beep();
                 Command.Listen();
             }
@@ -81,10 +107,18 @@ namespace Login_Server
         /// </summary>
         public static void Stop()
         {
-            _isRunning = false;
-            _loginServer.Stop();
-            _loginServer = null;
-            Environment.Exit(0);
+            try
+            {
+                _isRunning = false;
+                _loginServer.Stop();
+                _loginServer = null;
+                Environment.Exit(0);
+            }
+            catch
+            {
+                _isRunning = false;
+                Environment.Exit(0);
+            }
         }
     }
 }
